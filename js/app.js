c314ac23156b664bf1de787abc32d10812138b22
Edward Gao <egao@sent.com>
2016-12-17 02:30:08 -1000

    loads movies properly

--- js/app.js
+++ js/app.js
@@ -5,17 +5,23 @@ this.currentObj = {
   species: null
 };
 
+this.filmArr = {};
+
 this.filmObj = {};
 
-function getObject(link, callback) {
+this.planetObj = {};
+
+function getObject(obj, link, callback) {
   let oReq = new XMLHttpRequest();
   oReq.addEventListener('load', parseObj);
   oReq.open('GET', link);
   oReq.send();
   function parseObj() {
     let tempStr = this.responseText;
-    currentObj = JSON.parse(tempStr);
-    callback();
+    window[obj] = JSON.parse(tempStr);
+    if(typeof callback === 'function') {
+      callback();
+    }
   }
 }
 
@@ -26,24 +32,28 @@ function addTo(id, content, callback) {
   }
 }
 
-function getAndAdd(link, id, content, callback) {
-  getObject(link, () => {
-    addTo(id, currentObj[content], callback);
+function getAndAdd(obj, link, id, content, callback) {
+  getObject(obj, link, () => {
+    addTo(id, this[obj][content], callback);
   });
 }
 
 //getAndAdd("http://swapi.co/api/people/1/", 'person4Name', 'name');
 
 var sequence = [
+  'currentObj',
   'http://swapi.co/api/people/1/',
   'person4Name',
   'name',
+  'currentObj',
   '$currentObj.homeworld',
   'person4HomeWorld',
   'name',
+  'currentObj',
   'http://swapi.co/api/people/14/',
   'person14Name',
   'name',
+  'currentObj',
   '$currentObj.species',
   'person14Species',
   'name'
@@ -75,18 +85,41 @@ function runThru(func, param, delim) {
   eval(tempStr);
 }
 
-runThru('getAndAdd', sequence, 3);
+runThru('getAndAdd', sequence, 4);
 
-getObject('http://swapi.co/api/films/');
-
-document.getElementById('filmList').innerHtml += `
-<li class="film">
-  <h2 class="filmTitle"></h2>
-  <h3>Planets</h3>
-  <ul class="filmPlanets">
-    <li class="planet">
-      <h4 class="planetName"></h4>
-    </li>
-  </ul>
-</li>
-`;
+getObject('filmArr', 'http://swapi.co/api/films/', () => {
+  let filmList = document.getElementById('filmList');
+  filmArr = filmArr.results;
+  for(let i = 0; i < filmArr.length; i++) {
+    filmObj = filmArr[i];
+    // li
+    let li = document.createElement('li');
+    li.className = 'film';
+    // h2
+    let h2 = document.createElement('h2');
+    h2.className = 'filmTitle';
+    h2.innerText = filmObj.title;
+    // h3
+    let h3 = document.createElement('h3');
+    h2.innerText = 'Planets';
+    // ul
+    let ul = document.createElement('ul');
+    ul.className = 'filmPlanets';
+    for(let j = 0; j < filmObj.planets.length; j++) {
+      getObject('planetObj', filmObj.planets[j], () => {
+        let li = document.createElement('li');
+        li.className = 'planet';
+        let h4 = document.createElement('h4');
+        h4.className = 'planetName';
+        h4.innerText = planetObj.name;
+        li.appendChild(h4);
+        ul.appendChild(li);
+      });
+    }
+    // append
+    li.appendChild(h2);
+    li.appendChild(h3);
+    li.appendChild(ul);
+    filmList.appendChild(li);
+  }
+});
